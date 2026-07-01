8f4e9a8ea121f3fdaa02cfeb1cc19f5a76e8de13
Luis Hidalgo <hid21002@byui.edu>
2026-07-01 12:25:29 -0400

    Update contact page SEO copy

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
   return (
     <div>
       <SEO
-        title="Contact Triangle Adventures | Raleigh & Clayton E-Bike Tours"
-        description="Contact Triangle Adventures for guided e-bike tours, private groups, corporate events, special requests, and questions about Raleigh, Clayton, and Triangle area adventures."
+        title="Contact Triangle Adventures | Raleigh, Clayton & Wilmington E-Bike Tours"
+        description="Contact Triangle Adventures for guided e-bike tours, private groups, corporate events, special requests, and questions about Raleigh, Clayton, Wilmington, and Triangle area adventures."
         path="/contact-us"
       />
       <JsonLd
