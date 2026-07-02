db49390b0379901ec7292e03dd370bf785634f09
Luis Hidalgo <hid21002@byui.edu>
2026-07-02 06:57:46 -0400

    Use inline social icons in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import { contactLinks, socialLinks } from '../../data/bookingLinks';
 import { trackEmailClick, trackEvent, trackInstagramClick, trackPhoneClick } from '../../utils/analytics';
 
+const FacebookIcon = () => (
+  <svg className="footer--social-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+    <path d="M14.2 8.4V6.7c0-.8.5-1 1-1h1.5V3.1c-.7-.1-1.5-.2-2.3-.2-2.4 0-4 1.5-4 4.2v1.3H7.8v3h2.6V21h3.2v-9.6h2.7l.4-3h-3Z" />
+  </svg>
+)
+
+const InstagramIcon = () => (
+  <svg className="footer--social-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+    <path d="M7.8 2.8h8.4c2.8 0 5 2.2 5 5v8.4c0 2.8-2.2 5-5 5H7.8c-2.8 0-5-2.2-5-5V7.8c0-2.8 2.2-5 5-5Zm0 1.8c-1.8 0-3.2 1.4-3.2 3.2v8.4c0 1.8 1.4 3.2 3.2 3.2h8.4c1.8 0 3.2-1.4 3.2-3.2V7.8c0-1.8-1.4-3.2-3.2-3.2H7.8Zm4.2 3.1a4.3 4.3 0 1 1 0 8.6 4.3 4.3 0 0 1 0-8.6Zm0 1.8a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Zm4.6-2.2a1 1 0 1 1 0 2.1 1 1 0 0 1 0-2.1Z" />
+  </svg>
+)
+
 const Footer = () => {
   return (
     <footer className="site-footer">
@@ -26,7 +38,7 @@ const Footer = () => {
                   })
                 }
               >
-                <i className="fab fa-facebook-f"></i>
+                <FacebookIcon />
               </a>
               <a
                 href={socialLinks.instagram}
@@ -35,7 +47,7 @@ const Footer = () => {
                 aria-label="Triangle Adventures on Instagram"
                 onClick={() => trackInstagramClick({ buttonLocation: 'footer' })}
               >
-                <i className="fab fa-instagram"></i>
+                <InstagramIcon />
               </a>
             </div>
           </div>
